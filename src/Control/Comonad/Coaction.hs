0e7b9886ecd9a3e0e509411335f7ac9ef2b9173f
noiioiu <noiioiu@cocaine.ninja>
2024-09-22 07:36:13 -0500

    replace coact with duplicate

--- src/Control/Comonad/Coaction.hs
+++ src/Control/Comonad/Coaction.hs
@@ -20,22 +20,22 @@ import Data.Functor.Compose (Compose (..))
 
 -- | Instances must satisfy the following laws:
 --
--- * @'duplicate' '.' 'lcoact' = 'fmap' 'lcoact' '.' 'lcoact'@
+-- * @'duplicate' '.' 'lduplicate' = 'fmap' 'lduplicate' '.' 'lduplicate'@
 --
--- * @'extract' '.' 'lcoact' = 'id'@
+-- * @'extract' '.' 'lduplicate' = 'id'@
 class (Comonad w, Functor f) => LeftComodule w f where
-  lcoact ::
+  lduplicate ::
     f a ->
     -- | left comonad coaction
     w (f a)
 
 -- | Instances must satisfy the following laws:
 --
--- * @'fmap' 'duplicate' '.' 'rcoact' = 'rcoact' '.' 'rcoact'@
+-- * @'fmap' 'duplicate' '.' 'rduplicate' = 'rduplicate' '.' 'rduplicate'@
 --
--- * @'fmap' 'extract' '.' 'lcoact' = 'id'@
+-- * @'fmap' 'extract' '.' 'lduplicate' = 'id'@
 class (Comonad w, Functor f) => RightComodule w f where
-  rcoact ::
+  rduplicate ::
     f a ->
     -- | right comonad coaction
     f (w a)
@@ -43,37 +43,37 @@ class (Comonad w, Functor f) => RightComodule w f where
 -- | Given two comonads r and s, an (r, s) bicomodule is a functor that is a left comodule over r and a right comodule over s, where the two coactions are compatible.
 --   Instances must satisfy the following law in addition to the laws for @'LeftComodule'@ and @'RightComodule'@:
 --
--- * @'lcoact' '.' 'rcoact' = 'fmap' 'rcoact' '.' 'lcoact' = 'bicoact'@
+-- * @'lduplicate' '.' 'rduplicate' = 'fmap' 'rduplicate' '.' 'lduplicate' = 'bicoact'@
 class (LeftComodule r f, RightComodule s f) => BiComodule r s f where
   bicoact ::
     f a ->
     -- | two-sided comonad coaction
     r (f (s a))
-  bicoact = lcoact . rcoact
+  bicoact = lduplicate . rduplicate
 
 instance (Comonad w) => LeftComodule w w where
-  lcoact = duplicate
+  lduplicate = duplicate
 
 instance (Comonad w) => RightComodule w w where
-  rcoact = duplicate
+  rduplicate = duplicate
 
 instance (Comonad w) => BiComodule w w w where
   bicoact = duplicate . duplicate
 
 instance (Comonad w) => LeftComodule Identity w where
-  lcoact = Identity
+  lduplicate = Identity
 
 instance (Comonad w) => RightComodule Identity w where
-  rcoact = fmap Identity
+  rduplicate = fmap Identity
 
 instance (Comonad w) => BiComodule Identity Identity w where
   bicoact = Identity . fmap Identity
 
 instance (Comonad w, Functor f, LeftComodule w v) => LeftComodule w (Compose v f) where
-  lcoact = fmap Compose . lcoact . getCompose
+  lduplicate = fmap Compose . lduplicate . getCompose
 
 instance (Comonad w, Functor f, RightComodule w v) => RightComodule w (Compose f v) where
-  rcoact = Compose . fmap rcoact . getCompose
+  rduplicate = Compose . fmap rduplicate . getCompose
 
 instance (Comonad s, Comonad t, Functor f, LeftComodule s u, RightComodule t v) => BiComodule s t (Compose u (Compose f v))
 
@@ -91,25 +91,25 @@ instance (Comonad s, Comonad t, Functor f, LeftComodule s u, RightComodule t v) 
 comonadTransLCoscale :: (Comonad w, ComonadTrans t, Comonad (t w)) => t w a -> w (t w a)
 comonadTransLCoscale = lower . duplicate
 
-instance (Comonad w) => LeftComodule w (IdentityT w) where lcoact = comonadTransLCoscale
+instance (Comonad w) => LeftComodule w (IdentityT w) where lduplicate = comonadTransLCoscale
 
-instance (Comonad w) => LeftComodule w (EnvT e w) where lcoact = comonadTransLCoscale
+instance (Comonad w) => LeftComodule w (EnvT e w) where lduplicate = comonadTransLCoscale
 
-instance (Comonad w) => LeftComodule w (StoreT s w) where lcoact = comonadTransLCoscale
+instance (Comonad w) => LeftComodule w (StoreT s w) where lduplicate = comonadTransLCoscale
 
-instance (Comonad w, Monoid m) => LeftComodule w (TracedT m w) where lcoact = comonadTransLCoscale
+instance (Comonad w, Monoid m) => LeftComodule w (TracedT m w) where lduplicate = comonadTransLCoscale
 
 -- | Default right scalar comultiplication for comonad transformers.
 comonadTransRCoscale :: (Comonad w, ComonadTrans t, Comonad (t w)) => t w a -> t w (w a)
 comonadTransRCoscale = fmap lower . duplicate
 
-instance (Comonad m) => RightComodule m (IdentityT m) where rcoact = comonadTransRCoscale
+instance (Comonad m) => RightComodule m (IdentityT m) where rduplicate = comonadTransRCoscale
 
-instance (Comonad m) => RightComodule m (EnvT e m) where rcoact = comonadTransRCoscale
+instance (Comonad m) => RightComodule m (EnvT e m) where rduplicate = comonadTransRCoscale
 
-instance (Comonad m) => RightComodule m (StoreT s m) where rcoact = comonadTransRCoscale
+instance (Comonad m) => RightComodule m (StoreT s m) where rduplicate = comonadTransRCoscale
 
-instance (Comonad w, Monoid m) => RightComodule w (TracedT m w) where rcoact = comonadTransRCoscale
+instance (Comonad w, Monoid m) => RightComodule w (TracedT m w) where rduplicate = comonadTransRCoscale
 
 -- | Default two-sided scalar comultiplication for comonad transformers.
 comonadTransBiCoscale :: (Comonad w, ComonadTrans t, Comonad (t w)) => t w a -> w (t w (w a))
